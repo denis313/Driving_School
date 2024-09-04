692f6b97adf1fd43d6de53ffd8a02802a756be64
evtus <evtushokdenis2004@gmail.com>
2024-09-04 11:03:35 +0300

    update

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -141,10 +141,10 @@ async def page_seven(callback: CallbackQuery):
     if user.doc is None:
         if callback.data == 'adult':
             adult = True
-            doc = FSInputFile('handlers/document.docx', filename='Образец Договора')
+            doc = FSInputFile('handlers/document.pdf', filename='Образец Договора')
         else:
             adult = False
-            doc = FSInputFile('handlers/Договор', filename='Образец Договора')
+            doc = FSInputFile('handlers/document.docx', filename='Образец Договора')
         await db_manager.update_user(user_id=callback.from_user.id, user_data={'adult': adult})
         await bot.send_document(chat_id=callback.message.chat.id,
                                 document=doc)
