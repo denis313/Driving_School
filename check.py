880356435b3f77737c13231254a878870b38ac8d
evtus <evtushokdenis2004@gmail.com>
2024-10-29 14:35:48 +0300

    update file

--- check.py
+++ check.py
@@ -22,15 +22,15 @@ async def check_pay():
             days = user.end_date
             end = days + timedelta(days=9)
             if days == now:
-                await bot.send_photo(photo=get_photo(name=10), chat_id=user.user_id, caption=lexicon['pay'], reply_markup=keyboard_parts())
+                await bot.send_photo(photo=get_photo(name='buy'), chat_id=user.user_id, caption=lexicon['pay'], reply_markup=keyboard_parts())
             elif days < now < end:
-                await bot.send_photo(photo=get_photo(name=10), chat_id=user.user_id,
+                await bot.send_photo(photo=get_photo(name='buy'), chat_id=user.user_id,
                                      caption=lexicon['pay_urgently'].format(day=(end+timedelta(days=1)-now).days), reply_markup=keyboard_parts())
             elif end == now:
-                await bot.send_photo(photo=get_photo(name=10), chat_id=user.user_id, caption=lexicon['day_9'],
+                await bot.send_photo(photo=get_photo(name='buy'), chat_id=user.user_id, caption=lexicon['day_9'],
                                  reply_markup=keyboard_parts())
             elif (end + timedelta(days=1)) <= now:
-                await bot.send_photo(photo=get_photo(name=10), chat_id=user.user_id, caption=lexicon['del_user'])
+                await bot.send_photo(photo=get_photo(name='buy'), chat_id=user.user_id, caption=lexicon['del_user'])
                 await bot.send_message(chat_id=admin_id(),
                                        text=lexicon['for_admin_4'].format(user_phone=user.phone))
                 await db_manager.delete_user(user_id=user.user_id)
